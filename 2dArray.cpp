5c890204626f00279b8c6730147aabb3856e032e
Hakan Karimi <43370132+TheHakan@users.noreply.github.com>
2022-05-16 16:37:37 +0300

    Add files via upload

---
+++ 2dArray.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{   
+    /*
+	int i,j;
+	int arr[3][5]={{3,6,8,9,10},
+				   {5,10,23,56,90},
+				   {7,90,100,3,4}};
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			printf("%d ",arr[i][j]);
+		}
+	printf("\n");	
+	}
+	*/
+	
+	
+}
