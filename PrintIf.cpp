3c4b269fdf0bafce420725272f23c7353ad90f3a
Hakan Karimi <43370132+TheHakan@users.noreply.github.com>
2022-05-16 14:45:55 +0300

    Add files via upload

---
+++ PrintIf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	float c;
+	a=15;
+	b=a/2;
+	c=15.34567;
+	printf("%7.3f\n",c);
+	printf("%7.3f\n",c);
+	printf("%07.3f\n",c);
+	printf("%.3f\n",c);
+	printf("%2.3f\n",c); //ayn
+ 
+}
